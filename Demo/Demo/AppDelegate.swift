29443375c0118cc8c88d139fbe87d4f9de654fb7
liyongjie <iyongjie@yeah.com>
2020-05-11 10:10:48 +0800

    init

---
+++ Demo/Demo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Demo
+//
+//  Created by 李永杰 on 2020/5/11.
+//  Copyright © 2020 NewPath. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
---
+++ NPNavigationBar/NPNavigtionBar.swift
@@ -0,0 +1,21 @@
+//
+//  NPNavigtionBar.swift
+//  Demo
+//
+//  Created by 李永杰 on 2020/5/11.
+//  Copyright © 2020 NewPath. All rights reserved.
+//
+
+import UIKit
+
+class NPNavigtionBar: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
